9720fcfed6038519bb872d54dc9138631a84dd44
gulshanyadav01 <gulshany01@gmail.com>
2020-11-17 20:14:58 +0530

    yet to come

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-    <Header branding = "Contact Manaer" />
+    <Header branding = "Contact Manager" />
       <Contact name = "gulshan"
         phone = "8860098313"
         email = "gulshany01@gmail.com"
