021608d4ece7c8df99a7ed3805945d9bbb8524d2
gulshanyadav01 <gulshany01@gmail.com>
2020-11-18 08:54:49 +0530

    contacts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 
 import './App.css';
-import Contact from "./components/Contact"
+import Contacts from "./components/Contacts"
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
     <Header branding = "Contact Manager" />
-      <Contact name = "gulshan"
-        phone = "8860098313"
-        email = "gulshany01@gmail.com"
-      />
+      <Contacts />
     </div>
   );
 }
