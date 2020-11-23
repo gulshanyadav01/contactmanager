22033e67ef0ee40a4e67505d157b72021d9e2ec3
gulshanyadav01 <gulshany01@gmail.com>
2020-11-23 09:42:48 +0530

    final commit with the contect api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Contacts from "./components/Contacts/Contacts"
 import Header from "./components/Layout/Header";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; 
+import { HashRouter as Router, Route, Switch } from "react-router-dom"; 
 
 // import { Provider } from "./Context"
 import { Provider } from "./Context1"; 
@@ -13,13 +13,13 @@ import EditContact from "./components/Contacts/EditContact"
 function App() {
   return (
     <Provider>
-    <Router>
+    <Router basename = {process.env.PUBLIC_URL}>
       <div className="App">
         <Header branding = "Contact Manager" />
         <Switch>
           <Route exact path = '/' component = {Contacts}/>
           <Route exact path = "/AddContact" component = {AddContact}/>
-         <Route exact path = "/contact/edit/:id" component = {EditContact}/>
+          <Route exact path = "/contact/edit/:id" component = {EditContact}/>
           <Route exact path = "/test" component = {Test}/>
           <Route component ={NotFound}/>
         </Switch>
