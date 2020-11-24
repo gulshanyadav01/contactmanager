564b38e8ab0690b61a31ab4235f49931dd014a59
gulshanyadav01 <gulshany01@gmail.com>
2020-11-24 09:04:28 +0530

    add to contact

--- src/App2.js
+++ src/App2.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Provider } from './comp/Context'
 import Contacts from "./comp/Contacts"
+import AddContact from "./comp/AddContact"
 const  App2 = () =>  {
     return (
         <Provider>
             <div>
+                <AddContact/>
                <Contacts />
             </div>
         </Provider>
--- src/comp/AddContact.jsx
+++ src/comp/AddContact.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
-import AddContact from '../components/Contacts/AddContact'
+
 
 class AddContact extends Component {
+    state = {
+        name:"",
+        email:"",
+        phone:""
+    }
+
+    onChange = (e) => { 
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    onSubmit = (e) => { 
+        e.preventDefault(); 
+        console.log(this.state); 
+
+    }
+
+
     render() {
         return (
             <div>
-                
+                <form onSubmit = {this.onSubmit}>
+                    <input type = "text" name = "name" placeholder = 'enter name' value = {this.state.name}onChange = {this.onChange}/><br/>
+                    <input type = "text" name = "email" placeholder = 'enter email' value = {this.state.email}onChange = {this.onChange}/><br/>
+                    <input type = "text" name = "phone" placeholder = 'enter phone' value = {this.state.phone}onChange = {this.onChange}/><br/>
+                    <input type = "submit" value = "submit"/>
+                </form>
             </div>
         )
     }
