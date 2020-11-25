3a404550b3ae1529b900416cf44824dfcf8dae37
gulshanyadav01 <gulshany01@gmail.com>
2020-11-25 20:32:22 +0530

    yet to come

--- src/comp1/AddContact.jsx
+++ src/comp1/AddContact.jsx
@@ -35,14 +35,14 @@ class AddContact extends Component {
     
 
     render() {
-        // const { name, email, phone} = this.state;
+        const { name, email, phone} = this.state;
         return(
             <Consumer>
                 {value =>{
                 return (
                     <div>
                         <form onSubmit = {this.onSubmit.bind(this,value.dispatch)}>
-                            <input type = "text" name = "name" placeholder = "enter name" value = {this.state.name} onChange = {this.onChange}/><br/>
+                            <input type = "text" name = "name" placeholder = "enter name" value = {name} onChange = {this.onChange}/><br/>
                             <input type = "email" name = "email" placeholder = "enter email" value = {this.state.email} onChange = {this.onChange}/><br/>
                             <input type = "text" name = "phone" placeholder = "enter phone" value = {this.state.phone} onChange = {this.onChange}/><br/>
                             <input type = "submit" value ="submit"/>
