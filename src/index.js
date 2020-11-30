cc9d3f86f27f9a649842292301f3d565742747db
gulshanyadav01 <gulshany01@gmail.com>
2020-11-30 21:24:00 +0530

    yet to come

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
   <div>
-    <App/>
+    <App />
   </div>  
   </React.StrictMode>,
   document.getElementById('root')
