57a5c6a8e90afd5944ecca2c9b5eaa2193cae0f0
gulshanyadav01 <gulshany01@gmail.com>
2020-11-30 21:23:00 +0530

    yet to come

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css"
 
-import App from './App4';
+import App from './App3';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
