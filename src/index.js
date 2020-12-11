5b8631663cccee87cf4ee4e47a983bbf9d3beb56
gulshanyadav01 <gulshany01@gmail.com>
2020-12-11 18:42:49 +0530

    yet to come

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css"
 
-import App from './App3';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
   <div>
-    <App />
+    <App style = {{backgroundColor:"#182037"}} />
   </div>  
   </React.StrictMode>,
   document.getElementById('root')
